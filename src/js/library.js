bfc69caf3a2b3c7f1a55cefd0eb8d18f11315910
Yunus <sariyunus@proton.me>
2025-10-22 13:41:23 +0300

    header and svg stars

--- src/js/library.js
+++ src/js/library.js
@@ -27,11 +27,27 @@ function createStarRating(vote) {
 
   for (let i = 1; i <= 5; i++) {
     if (rating >= i) {
-      stars += `<svg class="star-svg-full" width="20" height="20"><use href="../img/icon.svg#icon-full-star"></use></svg>`;
+      stars += `<svg class="star-svg-full" width="20" height="20"><use href="../img/icon.svg#icon-full-star"></svg>`;
     } else if (rating >= i - 0.5) {
-      stars += `<svg class="star-svg-half" width="20" height="20"><use href="../img/icon.svg#icon-half-star"></use></svg>`;
+      stars += `<svg class="star-svg-half" width="20" height="20" viewBox="0 0 32 32">
+                    <path
+                      fill="none"
+                      stroke="#f87719"
+                      stroke-linejoin="round"
+                      stroke-linecap="butt"
+                      stroke-miterlimit="4"
+                      stroke-width="2"
+                      d="M30 13h-10.75l-3.25-10-3.25 10h-10.75l8.75 6-3.375 10 8.625-6.25 8.625 6.25-3.375-10 8.75-6z">
+                    </path>
+
+                    <path
+                      d="M16 3v19.75l-8.625 6.25 3.375-10-8.75-6h10.75l3.25-10z"
+                      fill="#f87719">
+                    </path>
+                </svg>
+              `;
     } else {
-      stars += `<svg class="star-svg-empty" width="20" height="20"><use href="../img/icon.svg#icon-empty-star"></use></svg>`;
+      stars += `<svg class="star-svg-empty" width="20" height="20"><use href="../img/icon.svg#icon-empty-star"></svg>`;
     }
   }
 
@@ -334,6 +350,7 @@ async function loadGenres() {
 let GENRES = {};
 
 (async function init() {
+  await loadPartials();
   await loadGenres();
   fetchTrendingMovie();
   loadLibrary();
